13325fcaa851099f10c011be99fd73ed3cdd835c
x1b6e6 <ftdabcde@gmail.com>
2021-02-12 11:40:30 +0700

    install: add error info when trying run install.sh as shell script
    Signed-off-by: x1b6e6 <ftdabcde@gmail.com>

--- install.sh
+++ install.sh
@@ -63,4 +63,24 @@ keys_gen_install() {
 	)
 }
 
+# detect running from shell
+# https://stackoverflow.com/a/28776166
+sourced=0
+if [ -n "$ZSH_EVAL_CONTEXT" ]; then
+	case $ZSH_EVAL_CONTEXT in *:file) sourced=1;; esac
+elif [ -n "$KSH_VERSION" ]; then
+	[ "$(cd $(dirname -- $0) && pwd -P)/$(basename -- $0)" != "$(cd $(dirname -- ${.sh.file}) && pwd -P)/$(basename -- ${.sh.file})" ] && sourced=1
+elif [ -n "$BASH_VERSION" ]; then
+	(return 0 2>/dev/null) && sourced=1
+else # All other shells: examine $0 for known shell binary filenames
+	# Detects `sh` and `dash`; add additional shell filenames as needed.
+	case ${0##*/} in sh|dash) sourced=1;; esac
+fi
+
+if (( !sourced )); then
+	echo "This script for automate running while installation" >&2
+	echo "Please run 'makepkg -si' for build and install this package and dependencies" >&2
+	exit 1
+fi
+
 # vim: set ts=4 sw=0 noexpandtab autoindent :
